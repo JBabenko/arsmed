dc1100605bf3292127920a7c2738569193a79ca2
Юрий Бабенко <jury.babenko@maximumtest.ru>
2020-07-11 11:27:38 +0300

    Some changes

--- src/js/select.js
+++ src/js/select.js
@@ -4,28 +4,44 @@ $(document).ready(function() {
   $('.select__list').each(function() {
     $(this).find('.select__input').first().change().attr('checked', 'checked');
   });
+ 
+  function clickSelect() {
+    const productContainer = $(this).closest('.js-product-container');
 
-  
-function clickSelect() {
-  if ($(this).hasClass('select_opened')) {
-    $(this).removeClass('select_opened');
-    return;
-  }
-  $('.select').removeClass('select_opened');
-  $(this).addClass('select_opened');
-};
+    if ($(this).hasClass('select_opened')) {
+      $(this).removeClass('select_opened');
 
-$(document).mouseup(e => {
-  if (!$('.select').is(e.target) && $('.select').has(e.target).length === 0) {
+      if (productContainer.length) {
+        productContainer.css('z-index', '');
+      }
+      return;
+    }
+
     $('.select').removeClass('select_opened');
-  }
-});
+    $('.js-product-container').css('z-index', '');
+    $(this).addClass('select_opened');
 
-function changeOption() {
-  const optionText = $(this).data('text');
-  const $container = $(this).closest('.select');
-  $container.find('.select__text').text(optionText);
-  $container.removeClass('select_opened');
-};
+    if (productContainer.length) {
+      productContainer.css('z-index', '2');
+    }
+  };
+
+  $(document).mouseup(e => {
+    if (!$('.select').is(e.target) && $('.select').has(e.target).length === 0) {
+      $('.select').removeClass('select_opened');
+      $('.js-product-container').css('z-index', '');
+    }
+  });
+
+  function changeOption() {
+    const productContainer = $(this).closest('.js-product-container');
+    const optionText = $(this).data('text');
+    const $container = $(this).closest('.select');
+    $container.find('.select__text').text(optionText);
+    $container.removeClass('select_opened');
+    if (productContainer.length) {
+      productContainer.css('z-index', '');
+    }
+  };
 
 });
